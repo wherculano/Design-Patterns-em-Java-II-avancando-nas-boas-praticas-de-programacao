227c217419ccf44a35e637bfa017de6e943556d6
Wagner Herculano <wagherculano@hotmail.com>
2023-05-03 16:16:40 -0300

    Código refatorado para aplicação do pattern Decorator

--- src/br/com/alura/loja/imposto/ICMS.java
+++ src/br/com/alura/loja/imposto/ICMS.java
@@ -1,13 +1,17 @@
 package br.com.alura.loja.imposto;
 
-import java.math.BigDecimal;
-
 import br.com.alura.loja.orcamento.Orcamento;
 
-public class ICMS implements Imposto {
+import java.math.BigDecimal;
 
-	public BigDecimal calcular(Orcamento orcamento) {
-		return orcamento.getValor().multiply(new BigDecimal("0.1"));
-	}
+
+public class ICMS extends Imposto {
+    public ICMS(Imposto outro) {
+        super(outro);
+    }
+
+    public BigDecimal realizarCalculo(Orcamento orcamento) {
+        return orcamento.getValor().multiply(new BigDecimal("0.1"));
+    }
 
 }
