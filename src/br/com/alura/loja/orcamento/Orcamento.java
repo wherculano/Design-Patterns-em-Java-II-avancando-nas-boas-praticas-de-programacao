2c213ae103b7767a9a336b4837e00e6cbf6e750f
Wagner Herculano <wagherculano@hotmail.com>
2023-05-03 11:26:29 -0300

    incluido metodo que verifica se o orçamento está finalizado ou não.

--- src/br/com/alura/loja/orcamento/Orcamento.java
+++ src/br/com/alura/loja/orcamento/Orcamento.java
@@ -3,51 +3,55 @@ package br.com.alura.loja.orcamento;
 import java.math.BigDecimal;
 
 import br.com.alura.loja.orcamento.situacao.EmAnalise;
+import br.com.alura.loja.orcamento.situacao.Finalizado;
 import br.com.alura.loja.orcamento.situacao.SituacaoOrcamento;
 
 public class Orcamento {
 
-	private BigDecimal valor;
-	private int quantidadeItens;
-	private SituacaoOrcamento situacao;
+    private BigDecimal valor;
+    private int quantidadeItens;
+    private SituacaoOrcamento situacao;
 
-	public Orcamento(BigDecimal valor, int quantidadeItens) {
-		this.valor = valor;
-		this.quantidadeItens = quantidadeItens;
-		this.situacao = new EmAnalise();
-	}
+    public Orcamento(BigDecimal valor, int quantidadeItens) {
+        this.valor = valor;
+        this.quantidadeItens = quantidadeItens;
+        this.situacao = new EmAnalise();
+    }
 
-	public void aplicarDescontoExtra() {
-		BigDecimal valorDescontoExtra = this.situacao.calcularDescontoExtra(this);
-		this.valor = this.valor.subtract(valorDescontoExtra);
-	}
-	
-	public void aprovar() {
-		this.situacao.aprovar(this);
-	}
-	
-	public void reprovar() {
-		this.situacao.reprovar(this);
-	}
-	
-	public void finalizar() {
-		this.situacao.finalizar(this);
-	}
+    public void aplicarDescontoExtra() {
+        BigDecimal valorDescontoExtra = this.situacao.calcularDescontoExtra(this);
+        this.valor = this.valor.subtract(valorDescontoExtra);
+    }
 
-	public BigDecimal getValor() {
-		return valor;
-	}
+    public void aprovar() {
+        this.situacao.aprovar(this);
+    }
 
-	public int getQuantidadeItens() {
-		return quantidadeItens;
-	}
+    public void reprovar() {
+        this.situacao.reprovar(this);
+    }
 
-	public SituacaoOrcamento getSituacao() {
-		return situacao;
-	}
-	
-	public void setSituacao(SituacaoOrcamento situacao) {
-		this.situacao = situacao;
-	}
+    public void finalizar() {
+        this.situacao.finalizar(this);
+    }
 
+    public BigDecimal getValor() {
+        return valor;
+    }
+
+    public int getQuantidadeItens() {
+        return quantidadeItens;
+    }
+
+    public SituacaoOrcamento getSituacao() {
+        return situacao;
+    }
+
+    public void setSituacao(SituacaoOrcamento situacao) {
+        this.situacao = situacao;
+    }
+
+    public boolean isFinalizado() {
+        return this.situacao instanceof Finalizado;
+    }
 }
